9a8b4a637dde6aa05f1415d15da8322f95827bed
enchanted <1655510745@qq.com>
2026-06-16 21:28:02 +0800

    去除缓冲池 直接对接数据 结构变化较大

--- app/src/main/java/com/qjj/screenshare/client/SocketClientThread.java
+++ app/src/main/java/com/qjj/screenshare/client/SocketClientThread.java
@@ -1,10 +1,8 @@
 package com.qjj.screenshare.client;
 
-import android.util.Log;
-
 import com.qjj.screenshare.entity.MessageEvent;
 import com.qjj.screenshare.MyApplication;
-import com.qjj.screenshare.entity.VideoPack;
+import com.qjj.screenshare.server.Decoder;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -15,14 +13,12 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
-import java.util.LinkedList;
 
 import static com.qjj.screenshare.MyApplication.CLOSE_CLIENT;
 import static com.qjj.screenshare.MyApplication.CONNECT_SERVER;
 import static com.qjj.screenshare.MyApplication.CONNECT_SERVER_ERROR;
-import static com.qjj.screenshare.MyApplication.CRC_FAIL;
 import static com.qjj.screenshare.MyApplication.CRC_OK;
-import static com.qjj.screenshare.MyApplication.LOST_PACK;
+import static com.qjj.screenshare.MyApplication.OPEN_FLOAT_WINDOW;
 import static com.qjj.screenshare.MyApplication.RECEIVE_DATA_ERROR;
 import static com.qjj.screenshare.MyApplication.TYPE1;
 
@@ -31,17 +27,13 @@ import static com.qjj.screenshare.MyApplication.TYPE1;
  */
 public class SocketClientThread extends Thread {
 
-    private int sumPack = 0;
-    private int lostPack = 0;
-
-    private LinkedList<VideoPack> linkedListVideo = new LinkedList<>();
-
     private Socket client;
     private boolean exit = false;
     private SocketAddress socketAddress;
     private InputStream inputStream;
     private OutputStream outputStream;
-    private VideoPlayThread videoPlayThread = null;
+    private Decoder videoDecoder;
+    private boolean hasInitVideo = false;
     private String ip = "";
 
     public SocketClientThread(String ip) {
@@ -64,9 +56,7 @@ public class SocketClientThread extends Thread {
         connect();
         if (!exit) {
             EventBus.getDefault().post(new MessageEvent(CONNECT_SERVER));
-
-            videoPlayThread = new VideoPlayThread(MyApplication.getSurface(), linkedListVideo);
-            videoPlayThread.start();
+            EventBus.getDefault().post(new MessageEvent(OPEN_FLOAT_WINDOW));
 
             try {
                 inputStream = client.getInputStream();
@@ -74,26 +64,27 @@ public class SocketClientThread extends Thread {
                 DataInputStream dis = new DataInputStream(inputStream);
 
                 while (!exit) {
-                    int crc = dis.readInt();
+                    dis.readInt(); // crc
                     int dataLength = dis.readInt();
                     long presentationTimeUs = dis.readLong();
-                    byte typeByte = dis.readByte();
-                    int type = (typeByte == TYPE1 ? 1 : 2);
+                    dis.readByte(); // type
 
                     byte[] videoPack = new byte[dataLength];
-                    dis.readFully(videoPack); // 确保读取完整的数据包
+                    dis.readFully(videoPack);
 
-                    synchronized (linkedListVideo) {
-                        outputStream.write(CRC_OK);
-                        sumPack++;
-                        if (linkedListVideo.size() >= 24) {
-                            lostPack++;
-                            EventBus.getDefault().post(new MessageEvent(LOST_PACK, "client:" + lostPack * 100 / sumPack + "%"));
-                            linkedListVideo.removeLast();
+                    if (!hasInitVideo) {
+                        videoDecoder = new Decoder(MyApplication.width, MyApplication.height, MyApplication.videoFrameRate, MyApplication.getSurface());
+                        if (videoDecoder.initDecoder()) {
+                            hasInitVideo = true;
+                        } else {
+                            break;
                         }
-                        VideoPack videoPack1 = new VideoPack(videoPack, type, presentationTimeUs);
-                        linkedListVideo.push(videoPack1);
                     }
+
+                    if (hasInitVideo) {
+                        videoDecoder.onFrame(videoPack, 0, dataLength, presentationTimeUs);
+                    }
+                    outputStream.write(CRC_OK);
                 }
             } catch (Throwable e) {
                 e.printStackTrace();
@@ -118,8 +109,8 @@ public class SocketClientThread extends Thread {
         } catch (Throwable e) {
             e.printStackTrace();
         }
-        if (videoPlayThread != null) {
-            videoPlayThread.exit();
+        if (videoDecoder != null) {
+            videoDecoder.release();
         }
         EventBus.getDefault().post(new MessageEvent(CLOSE_CLIENT));
         super.interrupt();
